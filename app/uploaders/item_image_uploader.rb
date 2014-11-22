a00cfa424ea23a1e483725c4bcc3bf5800041e06
kimler <kimler@gmail.com>
2014-11-21 19:57:12 -0500

    Adjust

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -3,7 +3,7 @@
 class ItemImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -16,11 +16,6 @@ class ItemImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  def cache_dir
-        "#{Rails.root}/tmp/uploads"
-  end
-  
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
