9d42fc4194af68cd97676c0b76b56bee6f4f57a8
kimler <kimler@gmail.com>
2014-11-21 23:42:28 -0500

    Ignore logs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-acts_as_orderer
-  
+
+  #attr_accessible :piggybak_sellable_attributes
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  acts_as_orderer
 
 end
